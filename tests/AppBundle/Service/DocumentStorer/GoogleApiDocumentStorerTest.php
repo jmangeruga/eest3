1cb221a5c5f52d211dd207952a2c989d01b4eb18
Jorge Mangeruga <j.a.mangeruga@gmail.com>
2017-03-07 01:04:08 -0300

    Added phpunit and dummy test case.

---
+++ tests/AppBundle/Service/DocumentStorer/GoogleApiDocumentStorerTest.php
@@ -0,0 +1,13 @@
+<?php    
+namespace Tests\AppBundle\Service\DocumentStorer;
+
+use AppBundle\Service\DocumentStorer\GoogleApiDocumentStorer;
+use PHPUnit\Framework\TestCase;
+
+class GoogleDriveDocumentStorer extends TestCase
+{
+    public function dummyTest()
+    {
+        $this->assertEquals(1,1);
+    }
+}
